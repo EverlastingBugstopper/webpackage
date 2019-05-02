af17a03ee3dd3b8d3388bff4c8ad5fabb3874002
Avery Harnish <averyharnish@gmail.com>
2019-05-02 16:44:19 -0400

    Adds support for URI input to dump-signedexchange

--- go/signedexchange/cmd/dump-signedexchange/main.go
+++ go/signedexchange/cmd/dump-signedexchange/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"time"
 
@@ -13,41 +14,63 @@ import (
 
 var (
 	flagInput     = flag.String("i", "", "Signed-exchange input file")
-	flagSignature = flag.Bool("signature", false, "Print only signature value")
+	flagURI       = flag.String("u", "", "Signed-exchange uri")
+	flagSignature = flag.Bool("signature", false, "Print signature value")
 	flagVerify    = flag.Bool("verify", false, "Perform signature verification")
 	flagCert      = flag.String("cert", "", "Certificate CBOR file. If specified, used instead of fetching from signature's cert-url")
+	flagHeaders   = flag.Bool("headers", true, "Print headers")
+	flagPayload   = flag.Bool("payload", false, "Print payload")
 )
 
 func run() error {
-	in := os.Stdin
+	var e *signedexchange.Exchange
 	if *flagInput != "" {
-		var err error
-		in, err = os.Open(*flagInput)
+		in, err := os.Open(*flagInput)
 		if err != nil {
 			return err
 		}
 		defer in.Close()
-	}
-
-	e, err := signedexchange.ReadExchange(in)
-	if err != nil {
-		return err
-	}
-
-	if *flagSignature {
-		fmt.Println(e.SignatureHeaderValue)
-		return nil
-	}
-
-	if *flagVerify {
-		if err := verify(e); err != nil {
+		e, err = signedexchange.ReadExchange(in)
+		if err != nil {
 			return err
 		}
-		fmt.Println()
+	} else if *flagURI != "" {
+		client := http.DefaultClient
+		req, err := http.NewRequest("GET", *flagURI, nil)
+		req.Header.Add("Accept", "application/signed-exchange;v=b3")
+		req.Header.Add("AMP-Cache-Transform", "google;v=1")
+		resp, err := client.Do(req)
+		if err != nil {
+			return err
+		}
+		defer resp.Body.Close()
+		e, err = signedexchange.ReadExchange(resp.Body)
+		if err != nil {
+			return err
+		}
 	}
+	if e != nil {
+		if *flagHeaders {
+			e.PrettyPrintHeaders(os.Stdout)
+		}
 
-	e.PrettyPrintHeaders(os.Stdout)
-	e.PrettyPrintPayload(os.Stdout)
+		if *flagPayload {
+			e.PrettyPrintPayload(os.Stdout)
+		}
+
+		if *flagSignature {
+			fmt.Println(e.SignatureHeaderValue)
+		}
+
+		if *flagVerify {
+			fmt.Println()
+			if err := verify(e); err != nil {
+				return err
+			}
+		}
+	} else {
+		fmt.Println("Need to pass -u with URI of sxg or -i with file location of sxg")
+	}
 
 	return nil
 }
@@ -72,7 +95,7 @@ func verify(e *signedexchange.Exchange) error {
 	verificationTime := time.Now()
 	if decodedPayload, ok := e.Verify(verificationTime, certFetcher, log.New(os.Stdout, "", 0)); ok {
 		e.Payload = decodedPayload
-		fmt.Println("The exchange has valid signature.")
+		fmt.Println("The exchange has a valid signature.")
 	}
 	return nil
 }
